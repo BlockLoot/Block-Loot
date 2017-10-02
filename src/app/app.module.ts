75ef9dcab09f1a79a8b7d792363b12b223bf063d
Ryan McCormick <ryan@itjobsguy.com>
2017-10-01 22:10:11 -0500

    import rxjs barrel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import './rxjs-operators';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
