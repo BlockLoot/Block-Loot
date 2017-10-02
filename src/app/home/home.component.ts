8ee3e5adebebb3f961becc29e1a1c9decb1d52a5
Ryan McCormick <ryan@itjobsguy.com>
2017-10-01 22:13:11 -0500

    fix data flow to provide filtered values from raw data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {Currency} from '../shared/models/currency.model';
-import {CoinMarketCapService} from '../data/coin-market-cap.service';
+import { Component, OnInit } from '@angular/core';
+import { Currency } from '../shared/models/currency.model';
+import { CoinMarketCapService } from '../data/coin-market-cap.service';
 
 @Component({
     selector: 'app-home',
@@ -9,30 +9,37 @@ import {CoinMarketCapService} from '../data/coin-market-cap.service';
 })
 export class HomeComponent implements OnInit {
     currencyData: Currency[];
-    currencyKeysToDisplay: string[] = [];
+    currencyKeysToDisplay: string[];
 
-    constructor(private coinMarketCapService: CoinMarketCapService) {
-    }
+    constructor(private coinMarketCapService: CoinMarketCapService) { }
 
     ngOnInit() {
         this.currencyKeysToDisplay = this.coinMarketCapService.currencyKeysToDisplay;
         this.loadCurrencyData();
     }
 
+    /**
+     * Return a filtered version of the set data
+     * @returns {Currency[]}
+     */
+    get filteredCurrencyData(): Currency[] {
+      if (this.currencyData) {
+        return this.currencyData.filter((item: Currency) => {
+          const acceptableKeys = this.currencyKeysToDisplay || [];
+          const el = item;
+
+          // Add undefined check to pass tests
+          if (typeof el.name !== 'undefined') {
+            if (acceptableKeys.indexOf(el.name.toLowerCase()) > -1) {
+              return item;
+            }
+          }
+        });
+      }
+    }
+
     private loadCurrencyData(): void {
         this.coinMarketCapService.getAllCurrencyData()
-            .subscribe(
-                data => this.updateCurrencyData(data)
-            );
+          .subscribe(data => this.currencyData = data);
     }
-
-    private updateCurrencyData(currencyData: Currency[]): void {
-        this.currencyData = currencyData;
-        const currencyKeysToDisplay = this.currencyKeysToDisplay;
-
-        this.currencyData = this.currencyData.filter(function (el) {
-            return currencyKeysToDisplay.indexOf(el.name.toLowerCase()) > -1;
-        });
-    }
-
 }
