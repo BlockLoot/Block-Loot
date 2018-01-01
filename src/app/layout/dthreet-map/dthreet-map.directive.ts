35e5661793d419474115eb887f65ad6c30ef2e7a
Ryan McCormick <ryan@itjobsguy.com>
2018-01-01 14:28:05 -0600

    update runner config and add more boilerplate

--- src/app/layout/dthreet-map/dthreet-map.directive.ts
+++ src/app/layout/dthreet-map/dthreet-map.directive.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import * as d3 from 'd3';
 
-import { d3 } from 'd3';
+
+// import { d3 } from 'd3';
 
 @Directive({
   selector: '[appDthreetMap]'
