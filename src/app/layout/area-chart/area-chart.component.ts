66f8328637cec5a2ed0694ecb0b0f0f5b71c032d
AlexKopen <alexkopen@gmail.com>
2018-01-10 23:15:21 -0600

    drew a line chart!

--- src/app/layout/area-chart/area-chart.component.ts
+++ src/app/layout/area-chart/area-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-area-chart',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AreaChartComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.drawLineChart();
+  }
+
+  private drawLineChart(): void {
+    const svg = d3.select('svg'),
+      margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = +svg.attr('width') - margin.left - margin.right,
+      height = +svg.attr('height') - margin.top - margin.bottom,
+      g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    const parseTime = d3.timeParse('%d-%b-%y');
+
+    const x = d3.scaleTime().rangeRound([0, width]);
+
+    const y = d3.scaleLinear().rangeRound([height, 0]);
+
+    const area = d3.area().x(function (d) {
+      return x(d.date);
+    })
+      .y1(function (d) {
+        return y(d.close);
+      });
+
+
+    d3.json('../../assets/data.json', function (error, data) {
+      if (error) {
+        throw error;
+      }
+
+      const chartData = data.map(function (item) {
+        item.date = parseTime(item.date);
+        item.close = +item.close;
+        return item;
+      });
+
+      x.domain(d3.extent(chartData, function (data) {
+        return data.date;
+      }));
+
+      y.domain([0, d3.max(chartData, function (data) {
+        return data.close;
+      })]);
+
+      area.y0(y(0));
+
+      g.append('path')
+        .datum(data)
+        .attr('fill', 'green')
+        .attr('d', area);
+    });
   }
 
 }
